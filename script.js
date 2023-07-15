afb5c1c0b084453b7c4e84403b1a509d05c1dc14
ShravanJadhav <shravanjadhav844@gmail.com>
2023-07-15 21:58:46 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,96 @@
+function getMenu() {
+    fetch('https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json')
+        .then(response => response.json())
+        .then(data => displayMenu(data))
+        .catch(error => console.log(error));
+}
+
+function displayMenu(foodItems) {
+
+     
+
+     
+    const menuElement = document.getElementById('menu');
+     
+    foodItems.forEach(item => {
+         const listItem = document.createElement('li');
+        
+        listItem.innerHTML= `
+        
+        <li class="item">
+        <img class = "imgClass" src="${item.imgSrc}" alt="">
+        <h1>${item.name} - $ ${item.price}</h1>
+         </li>`
+      
+         
+        menuElement.append(listItem);
+        
+    });
+   
+}
+
+
+function takeOrder() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+            const burgers = ['Cheeseburger', 'Chicken Burger', 'Veggie Burger'];
+            const order = {
+                burgers: []
+            };
+            for (let i = 0; i < 3; i++) {
+                const randomBurger = burgers[Math.floor(Math.random() * burgers.length)];
+                order.burgers.push(randomBurger);
+            }
+
+ 
+
+
+            resolve(order);
+        }, 2500);
+    });
+}
+
+
+function orderPrep() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ order_status: true, paid: false });
+        }, 1500);
+    });
+}
+
+function payOrder() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ order_status: true, paid: true });
+        }, 1000);
+    });
+}
+
+ 
+
+
+function thankyouFnc() {
+    alert('Thank you for eating with us today!');
+}
+
+
+
+async function handleOrder() {
+    try {
+        getMenu();
+        const order = await takeOrder();
+        console.log('Order:', order);
+        const orderStatus = await orderPrep();
+        console.log('Order Status:', orderStatus);
+        const paymentStatus = await payOrder();
+        console.log('Payment Status:', paymentStatus);
+        
+        thankyouFnc();
+        
+
+    } catch (error) {
+        console.log(error);
+    }
+}
